fd260c3d739c5475f21c7903aa547f6fc4d016a1
Layton Whiteley <layton.whiteley@gmail.com>
2018-03-21 09:27:03 -0500

    update test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,24 +3,28 @@ const tester = require('gitbook-tester');
 const { expect } = require('chai');
 
 const pkg = require('../package.json');
+const localPluginPath = path.join(__dirname, '..');
 
+function minifyHtml(content) {
+    return content
+        .replace(/\r?\n|\r/g, '')
+        .replace(/[\t ]+\</g, "<")
+        .replace(/\>[\t ]+\</g, "><")
+        .replace(/\>[\t ]+$/g, ">");
+}
 
 describe('codegroup', function () {
     it('should add code group of two', function () {
         return tester.builder()
             .withContent('{% codegroup %}\n{% codetab "sdk" %}\n```js\n var s = console1;\n```\n{% codetab "js" %}\n```js\n var s = console2;\n```{% endcodegroup %}')
-            .withLocalPlugin(path.join(__dirname, '..'))
+            .withLocalPlugin(localPluginPath)
             .withBookJson({
                 gitbook: pkg.engines.gitbook,
             })
             .create()
             .then(function (result) {
-                const html = result[0].content
-                    .replace(/\r?\n|\r/g, '')
-                    .replace(/[\t ]+\</g, "<")
-                    .replace(/\>[\t ]+\</g, "><")
-                    .replace(/\>[\t ]+$/g, ">");
-                
+                const html = minifyHtml(result[0].content);
+
                 expect(html).to.include('id="js-0');
                 expect(html).to.include('id="js-1');
                 expect(html).to.include('<div id="gbcg-tab-container"><div id="js-0');
